068b091e2fc88d6514eaa9565843623690a457b3
Fazilov Kamil <k.fazilov@icloud.com>
2021-02-20 11:36:28 +0300

    Изменил решение для Reverse Integer

--- Easy/reverse_integer.py
+++ Easy/reverse_integer.py
@@ -1,15 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        sign = 1 if x > 0 else -1
-        max_int = 2**31-1 
-        rev = 0
-
+        sign = 1 if x >= 0 else -1
         x = abs(x)
-        while x > 0:
-            last_digit = x % 10
-            x //= 10
-            if rev > (max_int - last_digit) / 10:
-                return 0
-            rev = rev * 10 + last_digit
-
-        return sign*rev
+        
+        rev = sign * int(str(x)[::-1])
+        
+        if rev < pow(-2, 31) or rev > pow(2, 31)-1:
+            return 0
+        else:
+            return rev
