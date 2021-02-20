473a74522fcf5fea3dea32fe241030a41267fc76
Fazilov Kamil <k.fazilov@icloud.com>
2021-02-20 11:39:04 +0300

    Изменил решение для Palindrome Number

--- Easy/palindrome_number.py
+++ Easy/palindrome_number.py
@@ -1,6 +1,3 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x >= 0 and str(x)[::-1] == str(x):
-            return True
-        else:
-            return False
+        return str(x)[::-1] == str(x)
