6aa56527b37e0625c085979fb9293a5e965aafdd
Fazilov Kamil <k.fazilov@icloud.com>
2021-02-20 12:00:12 +0300

    Изменил решение для Roman to Integer

--- Easy/roman_to_integer.py
+++ Easy/roman_to_integer.py
@@ -1,21 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         r_map = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000,
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
         }
-        num = [r_map[n] for n in s]
+        
+        num = [r_map[i] for i in s]
+        
         for i in range(len(num)):
             try:
                 if num[i] < num[i+1]:
                     num[i] = num[i+1]-num[i]
-                    num.pop(i+1)
+                    del num[i+1]
             except IndexError:
                 continue
-
+                
         return sum(num)
