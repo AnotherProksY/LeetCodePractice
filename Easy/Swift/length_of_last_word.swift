39651b9f6311be217876b6c33d5ec133023008c4
Fazilov Kamil <k.fazilov@icloud.com>
2021-03-13 00:21:27 +0300

    Удалил решения на Swift

--- Easy/Swift/length_of_last_word.swift
+++
@@ -1,9 +0,0 @@
-class Solution {
-    func lengthOfLastWord(_ s: String) -> Int {
-        if let word = s.split(separator: " ").last {
-            return word.count
-        } else {
-            return 0
-        }
-    }
-}
